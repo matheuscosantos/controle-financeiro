77970ec759cd27ed53058892cf3db395200b011d
Matheus <oliveiramatheus77@gmail.com>
2020-04-07 18:17:51 -0300

    Implementado Liquibase

--- src/main/java/com/estudos/controle_financeiro/ControleFinanceiroApplication.java
+++ src/main/java/com/estudos/controle_financeiro/ControleFinanceiroApplication.java
@@ -2,13 +2,16 @@ package com.estudos.controle_financeiro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication()
 @EnableCaching
 @EnableSwagger2
+@EntityScan(basePackageClasses = {ControleFinanceiroApplication.class, Jsr310JpaConverters.class})
 public class ControleFinanceiroApplication {
 
 	public static void main(String[] args) {
